6048994ff33811ca015b598cc511d9353874f3c0
Tom Ko <yungsu2391@gmail.com>
2023-10-01 00:57:17 +0900

    [ ADD ] scss 추가

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('/src'));
+
 app.get('/', require('./routes/controller/home.controller'));
+app.use('/api', require('./routes/api/index'));
 
 app.listen(3000, () => {
   console.log('3000 is running');
